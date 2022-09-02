4cdf7c0a5a6406d99f586086aa21d2ebb9290be2
Zachary Landry <landryzachary4@gmail.com>
2022-09-01 20:27:53 -0400

    part 4

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -14,9 +14,15 @@ function index () {
 function index (data) {
     let placesFormatted = data.places.map((place) => {
       return (
-        <div>
+        <div className="col-sm-6">
           <h2>{place.name}</h2>
+          <p className="text-center">
+            {place.cuisines}
+          </p>
           <img src={place.pic} alt={place.name}/>
+          <p className="text-center">
+            Located in {place.city}, {place.state}
+          </p>
         </div>
       )
     })
@@ -24,7 +30,9 @@ function index (data) {
       <Def>
           <main>
               <h1>Places to Rave About</h1>
-              {placesFormatted}
+              <div className="row">
+                {placesFormatted}
+              </div>
           </main>
       </Def>
   )
