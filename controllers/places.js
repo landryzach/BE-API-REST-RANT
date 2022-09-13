719686dd9aa6035fe1c47dc01dbc4e30b3fffaa4
Zachary Landry <landryzachary4@gmail.com>
2022-09-12 21:15:08 -0400

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -8,21 +8,13 @@ router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
+router.post('/', (req, res) => {
+  console.log(req.body)
+  res.send('POST /places')
+})
+
 // GET /places
-app.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/restaurant1.jpg'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/restaurant2.jpg'
-      }]      
+app.get('/', (req, res) => {  
       res.render('places/index', { places })
   })   
 
