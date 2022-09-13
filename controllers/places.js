1edeb5ae8a372aa1db0337f3d2cf4a9dc3477917
Zachary Landry <landryzachary4@gmail.com>
2022-09-12 21:25:40 -0400

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
 router.get('/', (req,res) => {
     res.send('GET /places')
@@ -8,13 +9,39 @@ router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
-router.post('/', (req, res) => {
-  console.log(req.body)
-  res.send('POST /places')
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show')
+  }
 })
 
+res.render('places/show', { place: places[id] })
+
+router.post('/', (req, res) => {
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
+})
+
+
 // GET /places
-app.get('/', (req, res) => {  
+router.get('/', (req, res) => {  
       res.render('places/index', { places })
   })   
 
